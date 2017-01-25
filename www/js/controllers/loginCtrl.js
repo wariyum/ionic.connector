4398a4ea41dba4631c045cd5e024263d7960241f
nazeels <nazeel.ak@gmail.com>
2017-01-25 19:26:38 +0400

    Logout is done

--- www/js/controllers/loginCtrl.js
+++ www/js/controllers/loginCtrl.js
@@ -3,10 +3,15 @@
 
 .controller('loginCtrl', loginCtrl);
 
-loginCtrl.$inject = ['$state'];
+loginCtrl.$inject = ['$state','appState','$scope'];
 
-function loginCtrl($state) {
+function loginCtrl($state,appState,$scope) {
 	var vm = this;
+
+	vm.credentials = appState.getLocalStorageAll();
+
+	vm.loggedIn = _isLoggedIn();
+	
 
 	vm.doLogin = function() {
 		alert('doLogin success');
@@ -14,5 +19,15 @@ function loginCtrl($state) {
 
 	vm.registration = function() {
 		$state.go('app.userRegister');
+	}
+
+	vm.logout = function function_name(argument) {
+		var credentials = appState.getLocalStorageAll();
+		appState.removeFromLocalStorage(credentials);
+		vm.loggedIn = _isLoggedIn();
+	}
+
+	function _isLoggedIn() {
+		return !(vm.credentials === undefined || vm.credentials.length === 0);
 	}
 }

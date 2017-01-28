7207e526464a07d94058c032a49543399b09e583
nazeels <nazeel.ak@gmail.com>
2017-01-28 14:55:57 +0400

    added product service

---
+++ www/js/services/productService.js
@@ -0,0 +1,47 @@
+(function() {
+    'use strict';
+
+    /**
+     * @ngdoc function
+     * @name app.service:homeService
+     * @description
+     * # homeService
+     * Service of the app
+     */
+
+    angular.module('wrConnector')
+        .service('productService', productService);
+
+    productService.$inject = ['$http','appService','appConstants','$state'];
+
+    function productService($http,appService,appConstants,$state) {
+        return {
+            getProductsPublished: getProductsPublished,
+            getProductsByCategoryId: getProductsByCategoryId,
+            getProductDetailsPublished: getProductDetailsPublished
+        };
+
+        function getProductsByCategoryId(categoryId) {
+              if(appConstants.mode === 'dev')
+                return $http.get(appService.getUrl()+'products-by-categoryId.json');       
+            else
+                return $http.get(appService.getUrl()+'connector/'+ appConstants.prog_id +'/categories/'+ categoryId+'/products');     
+        }
+
+        function getProductsPublished() {
+            if(appConstants.mode === 'dev')
+                return $http.get(appService.getUrl()+'published-products.json');
+            else
+                return $http.get(appService.getUrl() +'connector/'+ appConstants.prog_id + '/published/page');
+        }
+
+        function getProductDetailsPublished(productId) {
+            if(appConstants.mode === 'dev')
+                return $http.get(appService.getUrl()+'published-products.json');
+            else
+                return $http.get(appService.getUrl() +'connector/'+ appConstants.prog_id + '/product/' + productId);
+        }
+
+    }
+
+})();

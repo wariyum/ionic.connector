dba34eae38a273eea6fd3dd68602678bf44c6a2a
nazeels <nazeel.ak@gmail.com>
2017-02-17 23:26:42 +0400

    added moment.js

--- www/js/controllers/purchaseHistoryCtrl.js
+++ www/js/controllers/purchaseHistoryCtrl.js
@@ -2,14 +2,19 @@
 
 .controller('purchaseHistoryCtrl', purchaseHistoryCtrl);
 
-purchaseHistoryCtrl.$inject = ['cartService','moment'];
+purchaseHistoryCtrl.$inject = ['cartService','moment','lodash'];
 
-function purchaseHistoryCtrl(cartService,moment) {
+function purchaseHistoryCtrl(cartService,moment,_) {
 	var vm = this;
 	vm.test = moment("20111031", "YYYYMMDD").fromNow();
 	vm.init = function () {
 	 cartService.getOrderHistory().then(function (response){
+	 	_.forEach(response.data.success.content, function(value, key) {
+	 		// value.
+		   value.tmpMoment = moment(value.createdDateTime).fromNow();
+		});
 	 	vm.orderList = response.data.success.content;
+
 	 });
 	}
 }

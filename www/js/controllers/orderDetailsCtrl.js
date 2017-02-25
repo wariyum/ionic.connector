67c892cb90e7ce72d0de99f629b60fd44953ced1
nazeels <nazeel.ak@gmail.com>
2017-02-25 10:45:45 +0400

    Order cancellation in progress

--- www/js/controllers/orderDetailsCtrl.js
+++ www/js/controllers/orderDetailsCtrl.js
@@ -1,53 +1,52 @@
-
- angular.module('starter')
+angular.module('starter')
 
 .controller('orderDetailsCtrl', orderDetailsCtrl);
 
-orderDetailsCtrl.$inject = ['$stateParams','$rootScope','cartService','$scope','$ionicPopup'];
+orderDetailsCtrl.$inject = ['$stateParams', '$rootScope', 'cartService', '$scope', '$ionicPopup'];
 
-function orderDetailsCtrl($stateParams,$rootScope,cartService,$scope,$ionicPopup) {
-	var vm = this;
+function orderDetailsCtrl($stateParams, $rootScope, cartService, $scope, $ionicPopup) {
+    var vm = this;
 
-	vm.init = function() {
-		getParamsAndLoadPage();
-	}
+    vm.init = function() {
+        getParamsAndLoadPage();
+    }
 
-	 $rootScope.$on('rootScope:orderDetails', function (event, data) {
-		getParamsAndLoadPage();
-	  });
+    $rootScope.$on('rootScope:orderDetails', function(event, data) {
+        getParamsAndLoadPage();
+    });
 
-	 function getParamsAndLoadPage() {
-	 	vm.programId = $stateParams.programId;
-		vm.orderId = $stateParams.orderId;
-		//get Order details
-		 cartService.getOrderDetail(vm.orderId).then(function(response) {
-             // _.forEach(response.data.success.content, function(value, key) {
-             //     value.tmpMoment = moment(value.createdDateTime).fromNow();
-             // });
-             vm.orderDetails = response.data.success;
-             vm.status = vm.orderDetails.status;
-         });
-	 }
+    function getParamsAndLoadPage() {
+        vm.programId = $stateParams.programId;
+        vm.orderId = $stateParams.orderId;
+        //get Order details
+        cartService.getOrderDetail(vm.orderId).then(function(response) {
+            // _.forEach(response.data.success.content, function(value, key) {
+            //     value.tmpMoment = moment(value.createdDateTime).fromNow();
+            // });
+            vm.orderDetails = response.data.success;
+            vm.status = vm.orderDetails.status;
+        });
+    }
 
-	 // A confirm dialog
- $scope.showCancelConfirm = function() {
-   var confirmPopup = $ionicPopup.confirm({
-     title: 'Cancel your Order',
-     template: 'Are you sure you want to Cancel order?'
-   });
+    //Cancel Confirmation
+    $scope.showCancelConfirm = function() {
+        var confirmPopup = $ionicPopup.confirm({
+            title: 'Cancel your Order',
+            template: 'Are you sure you want to Cancel order?'
+        });
 
-   confirmPopup.then(function(res) {
-     if(res) {
-       cartService.cancelOrder(vm.orderId);
-     } else {
-       
-     }
-   });
- };
+        confirmPopup.then(function(res) {
+            if (res) {
+                cartService.cancelOrder(vm.orderId);
+            } else {
 
-	 vm.cancelOrder = function() {
-	 	$scope.showCancelConfirm();
-	 }
+            }
+        });
+    };
+
+    vm.cancelOrder = function() {
+        $scope.showCancelConfirm();
+    }
 
 
 }

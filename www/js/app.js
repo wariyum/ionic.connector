02835207c1be2a713860776b15fc35bbbf75df41
nazeels <nazeel.ak@gmail.com>
2017-03-18 08:22:29 +0400

    WIP: Push Notification for Android

--- www/js/app.js
+++ www/js/app.js
@@ -4,184 +4,213 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic','wrConnector','ngCordova','ngLodash','ionic-cache-src','ionic-toast','ngStorage','angularMoment'])
+angular.module('starter', ['ionic', 'wrConnector', 'ngCordova', 'ngLodash', 'ionic-cache-src', 'ionic-toast', 'ngStorage', 'angularMoment'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+.run(function($ionicPlatform, $cordovaPushV5) {
+    $ionicPlatform.ready(function() {
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
+        var options = {
+            android: {
+                senderID: "509484359572"
+            },
+            ios: {
+                alert: "true",
+                badge: "true",
+                sound: "true"
+            },
+            windows: {}
+        };
+
+        // initialize
+        $cordovaPushV5.initialize(options).then(function() {
+            // start listening for new notifications
+            $cordovaPushV5.onNotification();
+            // start listening for errors
+            $cordovaPushV5.onError();
+
+            // register to get registrationId
+            $cordovaPushV5.register().then(function(registrationId) {
+              console.log(registrationId);
+                // save `registrationId` somewhere;
+            })
+        });
+
+
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            cordova.plugins.Keyboard.disableScroll(true);
+
+        }
+        if (window.StatusBar) {
+            // org.apache.cordova.statusbar required
+            StatusBar.styleDefault();
+        }
+
+
+    });
 })
 
 
 //***** http Header
 .config(['$httpProvider', function($httpProvider) {
-   $httpProvider.interceptors.push('TokenInterceptor');
+    $httpProvider.interceptors.push('TokenInterceptor');
 }])
 
 .config(function($stateProvider, $urlRouterProvider) {
 
 
-  $stateProvider
+        $stateProvider
 
-    .state('app', {
-    url: '/app',
-    abstract: true,
-    templateUrl: 'templates/menu.html'
-  })
+            .state('app', {
+            url: '/app',
+            abstract: true,
+            templateUrl: 'templates/menu.html'
+        })
 
-  .state('app.search', {
-    url: '/search',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/search.html',
-        controller:'searchCtrl as vm'
-      }
-    }
-  })
+        .state('app.search', {
+            url: '/search',
+            views: {
+                'menuContent': {
+                    templateUrl: 'templates/search.html',
+                    controller: 'searchCtrl as vm'
+                }
+            }
+        })
 
-  .state('app.aboutUs', {
-      url: '/aboutUs',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/aboutUs.html',
-          controller:'aboutUsCtrl as vm'
-        }
-      }
-    })
-  .state('app.test', {
-      url: '/test',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/hardware.html',
-          controller:'hardwareCtrl as vm'
-        }
-      }
-    })
-  .state('app.forgetPassword', {
-      url: '/forgetPassword',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/forgetPassword.html',
-          controller:'forgetPasswordCtrl as vm'
-        }
-      }
-    })
-  .state('app.orderDetails', {
-      url: '/orderDetails/:programId/:orderId',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/orderDetail.html',
-          controller:'orderDetailsCtrl as vm'
-        }
-      }
-    })
-  .state('app.purchaseHistory', {
-      url: '/purchaseHistory',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/purchaseHistory.html',
-          controller:'purchaseHistoryCtrl as vm'
-        }
-      }
-    })
-    .state('app.products', {
-      url: '/products/:programId/:categoryId',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/productLists.html',
-          controller: 'productsCtrl as vm'
-        }
-      }
-    })
-    .state('app.login', {
-      url: '/login',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/login.html',
-          controller: 'loginCtrl as vm'
-        }
-      }
-    })
-    .state('app.checkout', {
-      url: '/checkout',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/checkout.html',
-          controller: 'checkoutCtrl as vm'
-        }
-      }
-    })
-    .state('app.userRegister', {
-      url: '/userRegister',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/userRegister.html',
-          controller: 'userRegisterCtrl as vm'
-        }
-      }
-    })
-    .state('app.shippingInfo', {
-      url: '/shippingInfo',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/shippingInfo.html',
-          controller: 'shippingInfoCtrl as vm'
-        }
-      }
-    })
-    .state('app.demo', {
-      url: '/demo',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/demo.html',
-          controller: 'demoCtrl as vm'
-          }
-      }
-    })
+        .state('app.aboutUs', {
+                url: '/aboutUs',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/aboutUs.html',
+                        controller: 'aboutUsCtrl as vm'
+                    }
+                }
+            })
+            .state('app.test', {
+                url: '/test',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/hardware.html',
+                        controller: 'hardwareCtrl as vm'
+                    }
+                }
+            })
+            .state('app.forgetPassword', {
+                url: '/forgetPassword',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/forgetPassword.html',
+                        controller: 'forgetPasswordCtrl as vm'
+                    }
+                }
+            })
+            .state('app.orderDetails', {
+                url: '/orderDetails/:programId/:orderId',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/orderDetail.html',
+                        controller: 'orderDetailsCtrl as vm'
+                    }
+                }
+            })
+            .state('app.purchaseHistory', {
+                url: '/purchaseHistory',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/purchaseHistory.html',
+                        controller: 'purchaseHistoryCtrl as vm'
+                    }
+                }
+            })
+            .state('app.products', {
+                url: '/products/:programId/:categoryId',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/productLists.html',
+                        controller: 'productsCtrl as vm'
+                    }
+                }
+            })
+            .state('app.login', {
+                url: '/login',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/login.html',
+                        controller: 'loginCtrl as vm'
+                    }
+                }
+            })
+            .state('app.checkout', {
+                url: '/checkout',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/checkout.html',
+                        controller: 'checkoutCtrl as vm'
+                    }
+                }
+            })
+            .state('app.userRegister', {
+                url: '/userRegister',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/userRegister.html',
+                        controller: 'userRegisterCtrl as vm'
+                    }
+                }
+            })
+            .state('app.shippingInfo', {
+                url: '/shippingInfo',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/shippingInfo.html',
+                        controller: 'shippingInfoCtrl as vm'
+                    }
+                }
+            })
+            .state('app.demo', {
+                url: '/demo',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/demo.html',
+                        controller: 'demoCtrl as vm'
+                    }
+                }
+            })
 
-  .state('app.single', {
-    url: '/productDetails/:productId',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/productDetail.html',
-        controller: 'productDetailCtrl as vm'
-      }
-    }
-  });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/demo');
-})
-.run(['$rootScope',function($rootScope){ // put the event handlers here 
-  $rootScope.$on('$stateChangeStart',function(event, toState, toParams, fromState, fromParams){
-  // console.log('$stateChangeStart to '+toState.to+'- fired when the transition begins. toState,toParams : \n',toState, toParams);
-});
-$rootScope.$on('$stateChangeError',function(event, toState, toParams, fromState, fromParams, error){
-  // console.log('$stateChangeError - fired when an error occurs during transition.');
-  // console.log(arguments);
-});
-$rootScope.$on('$stateChangeSuccess',function(event, toState, toParams, fromState, fromParams){
-  // console.log('$stateChangeSuccess to '+toState.name+'- fired once the state transition is complete.');
-});
-// $rootScope.$on('$viewContentLoading',function(event, viewConfig){
-//   // runs on individual scopes, so putting it in "run" doesn't work.
-//   console.log('$viewContentLoading - view begins loading - dom not rendered',viewConfig);
-// });
-$rootScope.$on('$viewContentLoaded',function(event){
-  // console.log('$viewContentLoaded - fired after dom rendered',event);
-});
-$rootScope.$on('$stateNotFound',function(event, unfoundState, fromState, fromParams){
-  // console.log('$stateNotFound '+unfoundState.to+'  - fired when a state cannot be found by its name.');
-  // console.log(unfoundState, fromState, fromParams);
-});
-}]);
-
+        .state('app.single', {
+            url: '/productDetails/:productId',
+            views: {
+                'menuContent': {
+                    templateUrl: 'templates/productDetail.html',
+                    controller: 'productDetailCtrl as vm'
+                }
+            }
+        });
+        // if none of the above states are matched, use this as the fallback
+        $urlRouterProvider.otherwise('/app/demo');
+    })
+    .run(['$rootScope', function($rootScope) { // put the event handlers here 
+        $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams) {
+            // console.log('$stateChangeStart to '+toState.to+'- fired when the transition begins. toState,toParams : \n',toState, toParams);
+        });
+        $rootScope.$on('$stateChangeError', function(event, toState, toParams, fromState, fromParams, error) {
+            // console.log('$stateChangeError - fired when an error occurs during transition.');
+            // console.log(arguments);
+        });
+        $rootScope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams) {
+            // console.log('$stateChangeSuccess to '+toState.name+'- fired once the state transition is complete.');
+        });
+        // $rootScope.$on('$viewContentLoading',function(event, viewConfig){
+        //   // runs on individual scopes, so putting it in "run" doesn't work.
+        //   console.log('$viewContentLoading - view begins loading - dom not rendered',viewConfig);
+        // });
+        $rootScope.$on('$viewContentLoaded', function(event) {
+            // console.log('$viewContentLoaded - fired after dom rendered',event);
+        });
+        $rootScope.$on('$stateNotFound', function(event, unfoundState, fromState, fromParams) {
+            // console.log('$stateNotFound '+unfoundState.to+'  - fired when a state cannot be found by its name.');
+            // console.log(unfoundState, fromState, fromParams);
+        });
+    }]);
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,21 +2,38 @@ angular.module('starter')
 
 .controller('appCtrl', appCtrl);
 
-appCtrl.$inject = ['categoryService','appService','$rootScope','cartService'];
+appCtrl.$inject = ['categoryService', 'appService', '$rootScope', 'cartService'];
 
-function appCtrl(categoryService,appService,$rootScope,cartService) {
-  var vm = this;
-  vm.init = init;
+function appCtrl(categoryService, appService, $rootScope, cartService) {
+    var vm = this;
+    vm.init = init;
 
-  function init() {
+    function init() {
 
-  	$rootScope.progId = appService.getProgId();
+        $rootScope.progId = appService.getProgId();
 
-    cartService.getCartItems();
+        cartService.getCartItems();
 
 
-    categoryService.getCategories(appService.getProgId()).then(function(response) {
+        categoryService.getCategories(appService.getProgId()).then(function(response) {
             $rootScope.categories = response.data.success;
         });
-  }
+    }
+
+    // triggered every time notification received
+    $rootScope.$on('$cordovaPushV5:notificationReceived', function(event, data) {
+    	
+        console.log(data.message);
+        console.log(data.title);
+        console.log(data.count);
+        console.log(data.sound);
+        console.log(data.image);
+        console.log(data.additionalData);
+    });
+
+    // triggered every time error occurs
+    $rootScope.$on('$cordovaPushV5:errorOcurred', function(event, e) {
+    	alert(error);
+        // e.message
+    });
 }

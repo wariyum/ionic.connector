7ec7a7dc583390e1d2b4f44b3bdd8ae984d49bfc
nazeels <nazeel.ak@gmail.com>
2017-03-24 17:31:49 +0400

    Added geo-location

--- www/js/controllers/shippingInfoCtrl.js
+++ www/js/controllers/shippingInfoCtrl.js
@@ -2,13 +2,26 @@ angular.module('starter')
 
 .controller('shippingInfoCtrl', shippingInfoCtrl);
 
-shippingInfoCtrl.$inject = ['cartService', '$ionicPopup', '$scope','$rootScope','$state'];
+shippingInfoCtrl.$inject = ['cartService', '$ionicPopup', '$scope', '$rootScope', '$state', '$cordovaGeolocation'];
 
-function shippingInfoCtrl(cartService, $ionicPopup, $scope,$rootScope,$state) {
+function shippingInfoCtrl(cartService, $ionicPopup, $scope, $rootScope, $state, $cordovaGeolocation) {
     var vm = this;
 
     vm.sendOrderToProcess = function() {
-       
+
+    }
+
+    vm.getGeoLocation = function() {
+        var posOptions = { timeout: 10000, enableHighAccuracy: false };
+        $cordovaGeolocation
+            .getCurrentPosition(posOptions)
+            .then(function(position) {
+                var lat = position.coords.latitude
+                var long = position.coords.longitude
+                alert(lat);
+            }, function(err) {
+                // error
+            });
     }
 
     vm.showConfirm = function() {
@@ -21,11 +34,11 @@ function shippingInfoCtrl(cartService, $ionicPopup, $scope,$rootScope,$state) {
 
         confirmPopup.then(function(res) {
             if (res) {
-            	 cartService.sendOrderToProcess();
-                 //broadcast event refresh Purchase Hisotry
+                cartService.sendOrderToProcess();
+                //broadcast event refresh Purchase Hisotry
 
-            $state.go('app.purchaseHistory');
-                 //redirect to purchase history
+                $state.go('app.purchaseHistory');
+                //redirect to purchase history
             } else {
                 //console.log('You are not sure');
             }
